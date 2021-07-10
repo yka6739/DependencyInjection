d847a0336d210daac8396a8d8d340fafffa6db3d
marsal <marsal@DESKTOP-SL3HUAK>
2021-07-10 14:25:34 +0530

    Add project files.

---
+++ DependencyInjection/Client/Resources/Services.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Client.Resources
+{
+    public class SingletonServices
+    {
+        public int value { get; set; }
+
+    }
+    public  class TransientServices
+    {
+        public int value { get; set; }
+    }
+}
